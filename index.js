5a93c1764f621f38c88f9b36a9a66405bd282f27
Dmitry Kichenko <dmitry@eventmobi.com>
2015-11-13 15:20:22 -0500

    Add YT search

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
+var rp = require('request-promise');
+var util = require('util');
+
 var PORT = process.env.PORT || 8080;
+var YT_VID_URL = "https://youtube.com/watch?v=";
+var YT_KEY = process.env.YT_KEY;
+// https://www.googleapis.com/youtube/v3/search?q=kids+hall&part=snippet&key=AIzaSyAD96gsoqxg9sx142kQrFqFe9dXGbalFN4
+var YT_URL_TEMPLATE = 'https://www.googleapis.com/youtube/v3/search?q=%s&part=snippet&key=%s';
+
+if (!YT_KEY) {
+	console.error('No YT API key provided');
+	process.exit(1);
+}
 
 var server = express();
 
@@ -16,10 +28,21 @@ function ytServer(req, res){
 
 	if (body) {
 		console.log('Incoming body', body);
-		console.log('Command', body.command, body.text);
 		response = {text: 'Echo back:' + body.text};
+		return searchYt(body.text)
+			.then(function(results) {
+				var firstResult = results.items[0];
+				var response = {
+					text: YT_VID_URL + firstResult.id.videoId,
+					//response_type: 'in_channel'
+				};
+				return res.json(response).end();
+			})
+			.catch(function(e) {
+				console.error('Failed to obtain results for', body.text, e);
+				res.status(500).end();
+			});
 
-		res.json(response).end();
 	} else {
 		console.error('Error parsing body');
 		res.status(500).end();
@@ -27,7 +50,12 @@ function ytServer(req, res){
 }
 
 function searchYt(term) {
-
+	var url = util.format(YT_URL_TEMPLATE, term, YT_KEY);
+	return rp(url)
+		.then(function(res) {
+			var results = JSON.parse(res);
+			return results;
+		});
 }
 
 server.listen(PORT);
