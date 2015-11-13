d03c523ab1ef32365de17d972141ea8300e8da6c
Dmitry Kichenko <dmitry@eventmobi.com>
2015-11-13 14:33:24 -0500

    Use express, for some reason

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
+var server = express();
+
+server.post(
+	'/ytsearch', 
+	bodyParser.json(),
+	ytServer
+);
+
 function ytServer(req, res){
-	var reqBody = '';
+	var body = req.body;	
 
-	function appendBody(data){
-		reqBody += data;
-	}
+	if (body) {
+		console.log('Incoming body', body);
+		console.log('Command', body.command, body.text);
+		response = {text: 'Echo back:' + body.text};
 
-	if (req.url.indexOf('/ytsearch') === 0) {
-		req.on('data', appendBody);
-		req.on('end', function() {
-			var body = parseBody(reqBody);	
-			var response;
-			var responseString;
-
-			if (body) {
-				console.log('Incoming body', body);
-				console.log('Command', body.command, body.text);
-				response = {text: 'Echo back:' + body.text};
-				responseString = JSON.stringify(response);
-
-				res.writeHead(
-					200,
-					{
-						'Content-Type': 'application/json',
-						'Content-Length': byteCount(responseString)
-					}
-				);
-				res.write(responseString);
-				res.end();
-			} else {
-				console.error('Error parsing body');
-				res.writeHead(500);
-				res.end();
-			}
-		});
+		res.json(response).end();
+	} else {
+		console.error('Error parsing body');
+		res.status(500).end();
 	}
 }
 
-function parseBody(body) {
-	var parsedBody;
-
-	try {
-		parsedBody = JSON.parse(body);
-	} catch (e) {
-	}
-
-	return parsedBody;
-}
-
-function byteCount(s) {
-    return encodeURI(s).split(/%..|./).length - 1;
-}
-
-http.createServer(ytServer).listen(PORT);
+server.listen(PORT);
 console.log('Started server on', PORT);
 
