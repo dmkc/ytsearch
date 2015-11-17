a7dbcca0cac5a32a76ff71791060bdfb9534ebe6
Dmitry Kichenko <dmitry@eventmobi.com>
2015-11-17 14:47:39 -0500

    Filter out non videos

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 var YT_VID_URL = "https://youtube.com/watch?v=";
 var YT_KEY = process.env.YT_KEY;
 // https://www.googleapis.com/youtube/v3/search?q=kids+hall&part=snippet&key=AIzaSyAD96gsoqxg9sx142kQrFqFe9dXGbalFN4
-var YT_URL_TEMPLATE = 'https://www.googleapis.com/youtube/v3/search?q=%s&part=snippet&key=%s';
+var YT_URL_TEMPLATE = 'https://www.googleapis.com/youtube/v3/search?q=%s&part=snippet&type=video&key=%s';
 
 if (!YT_KEY) {
 	console.error('No YT API key provided');
@@ -37,9 +37,12 @@ function ytServer(req, res){
 
 				if (!results.items.length) {
 					responseText = "Weird, I couldn't find any videos :(";
+
 				} else {
 					videoId = results.items[0].id.videoId;
-					console.info('Using first result', results.items[0]);
+					if (!videoId) {
+						console.error('No video ID found', results.items[0]);
+					}
 					responseText = YT_VID_URL + videoId;
 				}
 				response = {
